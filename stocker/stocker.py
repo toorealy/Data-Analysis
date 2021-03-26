6d0d8b3a52bdb933bde23962ffd00c20a4d86510
toorealy <j@s0n.email>
2021-03-25 21:00:55 -0500

    Docstrings and removed old code

--- stocker/stocker.py
+++ stocker/stocker.py
@@ -1,13 +1,30 @@
-from typing import List
-import pandas as pd
+from abc import ABC
 from datetime import datetime, timedelta
+from typing import List
+
+import pandas as pd
 import yfinance as yf
 
 
-class Stock:
+class Stock(ABC):
+    """
+    This is a class for capturing data from a single stock.
 
+    Attributes:
+        symbol (str): A stock ticker symbol.
+        start (str): A date in yyyy-mm-dd format. This data is the beginning of a period of time to query stock data.
+        end (str): A date in yyyy-mm-dd format. This data is the beginning of a period of time to query stock data.
+    """
     # Initialization requires a ticker symbol
-    def __init__(self, symbol: str, start: datetime=str((datetime.now()-timedelta(days=365))), end: datetime=str(datetime.now())):
+    def __init__(self, symbol: str, start: str=str((datetime.now()-timedelta(days=365))), end: str=str(datetime.now())):
+        """
+        The constructor for the Stock class.
+
+        Parameters:
+            symbol (str): A stock ticker symbol.
+            start (str): A date in yyyy-mm-dd format. This data is the beginning of a period of time to query stock data.
+            end (str): A date in yyyy-mm-dd format. This data is the beginning of a period of time to query stock data.
+        """
         # Enforce capitalization
         self._ticker = symbol.upper().split(" ")[0]
 
@@ -22,10 +39,13 @@ class Stock:
         return self._ticker + " from " + str(self.start_date) + " to " + str(self.end_date)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__} ({self.ticker!r}, start={self.start_date!r}, interval={self.end_date!r} )"
+        return f"{self.__class__.__name__} ({self.ticker!r}, start={self.start_date!r}, end={self.end_date!r} )"
 
         # Set various attributes
     def calculate_stats(self):
+        """
+        The method for calculating various metrics based on the stock's historic data.
+        """
         self.beta = self.stock_data.info['beta']
         c = self.history.tail(1)['Close'].item()
         o = self.history.head(1)['Open'].item()
@@ -83,7 +103,7 @@ class Stock:
     '''
 
     @property
-    def stock_data(self):
+    def stock_data(self) -> dict:
         return self._stock_data
 
     @stock_data.setter
@@ -92,7 +112,7 @@ class Stock:
         self._history = yf.download(tickers=self.ticker, start=self.start_date, end=self.end_date)
 
     @property
-    def history(self):
+    def history(self) -> pd.DataFrame:
         self._history['Close-Open'] = self._history['Close'] - self._history['Open']
         self._history['High-Low'] = self._history['High'] - self._history['Low']
         return self._history
@@ -101,27 +121,8 @@ class Stock:
     def history(self, history):
         self._history = history
 
-
     @property
-    def period(self):
-        return self._period
-
-    @period.setter
-    def period(self, period):
-        self._period = period
-        self.history = yf.download(tickers=self.ticker, start=self.start_date, end=self.end_date)
-
-    @property
-    def interval(self):
-        return self._interval
-
-    @interval.setter
-    def interval(self, interval):
-        self._interval = interval
-        self.history = yf.download(tickers=self.ticker, start=self.start_date, end=self.end_date)
-
-    @property
-    def ticker(self):
+    def ticker(self) -> str:
         return self._ticker
 
     @ticker.setter
@@ -130,10 +131,19 @@ class Stock:
         self.stock_data = yf.Ticker(symbol)
         self.calculate_stats()
 
-    def requery_data(self, start, end):
+    def requery_data(self, start: str, end: str) -> pd.DataFrame:
         return yf.download(tickers=self.ticker, start=start.split(" ")[0], end=end.split(" ")[0])
 
-class Market():
+class Market(ABC):
+    """
+    This is a class for putting together a basket of Stocks to represent a market.
+
+    Attributes:
+        symbol (str): A stock ticker symbol.
+        rf (str): A stock ticker whose performance is used as the "risk-free rate" for determining market performance.
+        start (str): A date in yyyy-mm-dd format. This data is the beginning of a period of time to query stock data.
+        end (str): A date in yyyy-mm-dd format. This data is the beginning of a period of time to query stock data.
+    """
     def __init__(self, symbols: List, rf: str='SPTI', start: datetime=str(datetime.now()), end: datetime=str((datetime.now()-timedelta(days=365)))):
         # Enforce capitalization
         tickers = [x.upper() for x in symbols]
